7915284118b93dd740b41a960f115ec73f254230
Gabriel Fontes <hi@m7.rs>
2026-08-01 11:15:46 -0300

    fix(hosts): go back to static timezone

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -3,13 +3,14 @@
     defaultLocale = lib.mkDefault "en_US.UTF-8";
     extraLocaleSettings = {
       LC_TIME = lib.mkDefault "pt_BR.UTF-8";
+      LC_MEASUREMENT = lib.mkDefault "pt_BR.UTF-8";
+      LC_MONETARY = lib.mkDefault "pt_BR.UTF-8";
     };
-    supportedLocales = lib.mkDefault [
+    extraLocales = lib.mkDefault [
       "en_US.UTF-8/UTF-8"
       "pt_BR.UTF-8/UTF-8"
     ];
   };
+  time.timeZone = lib.mkDefault "America/Sao_Paulo";
   location.provider = "geoclue2";
-  services.automatic-timezoned.enable = true;
-  systemd.services.automatic-timezoned.serviceConfig.Restart = "always";
 }
